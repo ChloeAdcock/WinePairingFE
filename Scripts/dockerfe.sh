4a6529edcf340672b9cf5d01e817a545c32bb751
ChloeAdcock <58259820+ChloeAdcock@users.noreply.github.com>
2020-02-06 17:28:25 +0000

    Delete dockerfe.sh

--- Scripts/dockerfe.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-./delete.sh
-sudo apt update
-git clone https://github.com/ChloeAdcock/WinePairingFE.git 
-cd WinePairingFE
-git checkout master
-docker build -t wines-frontend .
-docker run -dit --restart unless-stopped -d -p 80:80 --name wines-fe wines-frontend:latest
