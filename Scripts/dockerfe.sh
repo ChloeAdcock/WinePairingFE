812c909b81de7acccff595e8f30f85de210e08aa
ChloeAdcock <58259820+ChloeAdcock@users.noreply.github.com>
2020-02-06 17:29:47 +0000

    Update dockerfe.sh

--- Scripts/dockerfe.sh
+++ Scripts/dockerfe.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
-./delete.sh
+rm -rf WinePairingFE
+docker stop wines-fe
+docker rm wines-fe
+docker rmi wines-frontend
 sudo apt update
 git clone https://github.com/ChloeAdcock/WinePairingFE.git 
 cd WinePairingFE
 git checkout dev
 docker build -t wines-frontend .
 docker run -dit --restart unless-stopped -d -p 80:80 --name wines-fe wines-frontend:latest
+
